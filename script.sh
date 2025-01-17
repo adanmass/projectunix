941e6cb455349e1d1bb8f6a61b942c3aaefb573c
doaa <doaa@github>
2025-01-17 23:21:34 +0200

    rocket science

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ git add .
 git commit -m "rocket science" 
 git push 
 
-$JENKINS_TOKEN="11149d6e174dfb73096b8dca1efdcafd03"
+JENKINS_TOKEN="11149d6e174dfb73096b8dca1efdcafd03"
 
 curl -X POST "http://localhost:8080/job/unixjob/build?token=11149d6e174dfb73096b8dca1efdcafd03" \
     --user "weezo:$JENKINS_TOKEN" \
