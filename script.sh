99ba3736eec95776877f32846eb11902c58a5e7e
alayyad doaa <alayyadoaa@gmail.com>
2025-01-19 22:31:44 +0200

    script

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 git add .
-git commit -m "rocket science" 
+git commit -m "script commit" 
 git push 
 
-JENKINS_TOKEN="11149d6e174dfb73096b8dca1efdcafd03"
+JENKINS_TOKEN="11a8ec20ed25dc7c0979ddac1de1f1f875"
 
-curl -X POST "http://localhost:8080/job/unixjob/build?token=11149d6e174dfb73096b8dca1efdcafd03" \
-    --user "weezo:$JENKINS_TOKEN" \
+curl -X POST "http://localhost:8080/job/unixjob/build?token=11a8ec20ed25dc7c0979ddac1de1f1f875" \
+    --user "doaa:$JENKINS_TOKEN" \
     --fail
